b80fc753f243f19c11d69b12f8d0e235a7459d53
Javier Su <javier.su.weijie@gmail.com>
2018-06-29 18:14:07 +0800

    Added build scripts

---
+++ scripts/build-helloworld.sh
@@ -0,0 +1,1 @@
+../node_modules/assemblyscript/bin/asc ../contracts/helloworld/helloworld.ts -t ../build/untouched.wast -b ../build/untouched.wasm  --sourceMap --measure --noAssert --use abort=
---
+++ scripts/build-todo.sh
@@ -0,0 +1,1 @@
+../node_modules/assemblyscript/bin/asc ../contracts/todolist/todo.ts -t ../build/untouched.wast -b ../build/untouched.wasm  --sourceMap --measure --noAssert --use abort=
