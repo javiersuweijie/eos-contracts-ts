4e88f9a51620cf2e4c782414eb83dd0a825a2bb9
MaxGraey <maxgraey@gmail.com>
2018-06-30 23:26:43 +0300

    make Contract.dataStream static

--- contracts/contract.ts
+++ contracts/contract.ts
@@ -4,7 +4,7 @@ import {DataStream} from "./datastream";
 export class Contract {
   constructor(public receiver: u64) {}
 
-  get dataStream() : DataStream {
+  static get dataStream() : DataStream {
     let len = eos.action_data_size();
     let arr = new Uint8Array(len);
     read_action_data(changetype<usize>(arr.buffer), len);
